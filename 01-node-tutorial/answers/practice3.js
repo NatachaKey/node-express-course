0631d5f1d57479870d1e9d70663aa6d5a8de90da
Natacha Key <nataliaklueva@hotmail.com>
2023-03-14 11:06:37 +0100

    refactor task2

--- 01-node-tutorial/answers/practice3.js
+++ 01-node-tutorial/answers/practice3.js
@@ -1,21 +1,19 @@
 fs = require('fs').promises;
-const { writeFile }= require('fs').promises
-const { writeFileSync } = require('fs')
-
 
 const makeFile = async () => {
     try {
-        await writeFile('./content/practice3.txt',
-        `This is the first line. \n`,
-        {flag: 'a'})
-    
-    } catch(error) { 
-        console.log(error);
-    
-    }
-    for (let i = 2; i < 11; i++) {
-        writeFileSync('./content/practice3.txt', `This is line ${i}.\n`, { flag: 'a' })
-    }
-    }
+    await fs.writeFile('./content/practice2.txt', `This is the first line,\n`);
 
-    makeFile()
+    for (let i = 2; i < 11; i++) {
+    await fs.writeFile(
+        './content/practice2.txt', 
+        `This is line ${i}\n`,
+        { flag: 'a' })
+
+    }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  makeFile()
