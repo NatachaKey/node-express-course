488993b4b41c0eeec2c003854639757e41d38d2b
Natacha Key <nataliaklueva@hotmail.com>
2023-03-21 20:57:11 +0100

    update practice-middleware.js

--- 02-express-tutorial/practice-middleware.js
+++ 02-express-tutorial/practice-middleware.js
@@ -1,9 +1,3 @@
-// create a middleware function called consoleLog(req, res, next) that logs a statement to the console and then invokes the next function.
-//  Export this function. Edit practice-express.js to require the consoleLog function, and add an app.use statement so that it is invoked 
-//  before every request. Test the practice-express.js
-//  program to make sure the middleware is working, accessing your express program 
-//  from the browser using localhost:3000 as before.
-
 const express = require('express')
 
 const consoleLog = (req, res, next) => {
