71238bf6d828a99e47cfd02b079c9e8f93171cd0
Natacha Key <nataliaklueva@hotmail.com>
2023-03-23 20:54:24 +0100

    delete second package.json

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,36 +1,45 @@
-const express = require('express');
+const express = require('express')
 const app = express();
-const tasks = require('./routes/tasks');
-const connectDB = require('./db/connect');
-require('dotenv').config();
+const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
 
 //middleware
 
 //what does this line do?
-app.use(express.json());
+app.use(express.json())
 //The app.use() function adds a new middleware to the app. Express.json() is a built in middleware function, it parses incoming JSON requests and puts the parsed data in req.body.
 // parse converts json to JavaScript object
 //and this object is passed as a req.body
 
+
 //routes
-app.get('/hello', (req, res) => {
-  res.send('Task Manager app');
-});
+app.get('/hello', (req, res)=>{
+    res.send('Task Manager app')
+})
 
-app.use('/api/v1/tasks', tasks);
+app.use('/api/v1/tasks', tasks)
 
-const port = 3000;
 
-const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
-    app.listen(port, console.log(`Server is listening on port ${port}...`));
-  } catch (error) {
-    console.log(error);
-  }
-};
+const port = 3000
 
-start();
+const start = async () =>{
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening on port ${port}...`));
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+start()
+
+
+
+
+
+
 
 //TASK:
 //app.get('/api/v1/tasks') -get all the taskes
